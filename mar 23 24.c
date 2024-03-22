8751d628c2257c5f95fd0cea8bdebd7ff4e3b8f7
Md.Hasnatul Islam <147729301+hasnatulislam545460@users.noreply.github.com>
2024-03-23 03:09:27 +0600

    Add files via upload

--- mar 23 24.c
+++ mar 23 24.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main()
+{
+    char character;
+    /*gets or puts user tekeh line of text nei*/
+    printf("Enter any character: ");
+    /*eta use korle % use kora lage na*/
+    character = getchar();
+    printf("Assalamoualaikum ");
+    putchar(character);
+    return 0;
+}
