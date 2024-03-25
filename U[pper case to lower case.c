ca58bef20543a47f4dc062c52e638d1c78d37bb1
Md.Hasnatul Islam <147729301+hasnatulislam545460@users.noreply.github.com>
2024-03-26 01:05:18 +0600

    Add files via upload

---
+++ U[pper case to lower case.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+ {
+    char Upper;
+    printf("Enter any Uppercase letter: ");
+    scanf("%c",&Upper);
+    printf("The lower case letter: %c",Upper+32);
+    return 0;
+ }
