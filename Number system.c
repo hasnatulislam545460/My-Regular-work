638c623234c031c157ef9e6fa65d3703c3fbf5d4
Md.Hasnatul Islam <147729301+hasnatulislam545460@users.noreply.github.com>
2024-03-26 16:45:42 +0600

    Add files via upload

---
+++ Number system.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    //printf("Decimal Number: ");
+    printf("Octal Number: ");
+    //scanf("%d",&num);
+    scanf("%o",&num);
+    //printf("Octal Number: %o",num);
+    printf("Decimal Number: %d",num);
+    return 0;
+}
