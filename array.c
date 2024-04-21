715d4bf7713255edf29cce4fe851907390772acf
Md.Hasnatul Islam <147729301+hasnatulislam545460@users.noreply.github.com>
2024-04-21 23:38:02 +0600

    Add files via upload

---
+++ array.c
@@ -0,0 +1,11 @@
+// Array basic
+#include <stdio.h>
+
+int main()
+{
+    int Ar[5];
+    scanf("%d %d %d %d %d",&Ar[0],&Ar[1],&Ar[2],&Ar[3],&Ar[4]);
+    printf("\nArray : ");
+    printf("%d",Ar[2]);
+    return 0;
+}
