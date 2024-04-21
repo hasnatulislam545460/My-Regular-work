fbb5f1fd946abb120d8025fdd2e750b20d3eee35
Md.Hasnatul Islam <147729301+hasnatulislam545460@users.noreply.github.com>
2024-04-21 19:45:47 +0600

    Add files via upload

---
+++ even code.c
@@ -0,0 +1,15 @@
+//even number
+#include<stdio.h>
+int main()
+{
+    int i,sum=0;
+    //scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
