9ed1d4d5dba571059c3aa92e51e8a71482ad1311
Md.Hasnatul Islam <147729301+hasnatulislam545460@users.noreply.github.com>
2024-11-10 00:27:03 +0600

    Add files via upload

---
+++ circumferenceandarea.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int r;
+    double c,a;
+    scanf("%d",&r);
+    a=3.14159265358*r*r;
+    c=2*3.14159265358*r;
+    printf("%lf ",a);
+    printf("%lf",c);
+    return 0;
+
+}
---
+++ hallowin.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y,n,m,z;
+    scanf("%d %d",&n,&m);
+    scanf("%d %d",&x,&y);
+    z=(x*n)+(y*m);
+    printf("%d",z);
+    return 0;
+}
